ef1421f1de26ffe4bef1c4b20061df21a10ef03c
Artyom Myachin <sulfuraloxid@rambler.ru>
2020-03-10 12:15:28 +0600

    Task 1 completed

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <meta charset="utf8"/>
+        <title>World bank</title>
+        <link rel="stylesheet" href="src/css/style.css"/>
+    </head>
+    <body>
+        Test
+    </body>
+</html>
